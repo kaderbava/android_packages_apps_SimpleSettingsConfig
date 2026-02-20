f6ab626c3d443c03b69ef9e9a9177adb053f348b
kaderbava <ksbava7325@gmail.com>
2026-02-20 09:32:34 +0000

    Revert "Allow "lineage" settings to be set"
    * Infinity won't support lineage-sdk in A15.
    
    This reverts commit d63949dfe2184ac325a96803a69bad82adbb7a67.
    
    Change-Id: Idcb849ff1d59a182fde0cef96451c737614d69ee
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- src/org/lineageos/settingsconfig/BootReceiver.java
+++ src/org/lineageos/settingsconfig/BootReceiver.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2020 The Proton AOSP Project
- * Copyright (C) 2024-2026 The LineageOS Project
+ * Copyright (C) 2024-2025 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,8 +22,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
 import android.util.Log;
-
-import lineageos.providers.LineageSettings;
 
 import java.io.File;
 import java.io.IOException;
@@ -84,18 +82,6 @@ public class BootReceiver extends BroadcastReceiver {
 
                 case "system":
                     Settings.System.putString(context.getContentResolver(), key, value);
-                    break;
-
-                case "lineage_global":
-                    LineageSettings.Global.putString(context.getContentResolver(), key, value);
-                    break;
-
-                case "lineage_secure":
-                    LineageSettings.Secure.putString(context.getContentResolver(), key, value);
-                    break;
-
-                case "lineage_system":
-                    LineageSettings.System.putString(context.getContentResolver(), key, value);
                     break;
 
                 default:
